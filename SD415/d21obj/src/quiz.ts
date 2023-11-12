fc9fd66689d9341072d43f558574fc044167e903
meleshel <143976753+meleshel@users.noreply.github.com>
2023-11-12 17:46:14 -0500

    updates

--- SD415/d21obj/src/quiz.ts
+++ SD415/d21obj/src/quiz.ts
@@ -1,61 +1,116 @@
 
 
+// export interface StudentQuiz {
+//     id: number;
+//     quizAnswers: number[];
+//   }
+   
+//   export interface StudentResult {
+//     id: number;
+//     score: number;
+//   }
+   
+//   export const studentQuizzes: StudentQuiz[] = [];
+//   export const CORRECT_ANSWERS: number[] = [1, 2, 3];
+//   export function gradeStudent(
+//     studentAnswers: number[],
+//     correct: number[]
+//   ): number {
+//     let answers: number = 0;
+//     for (let i = 0; i < studentAnswers.length; i++) {
+//       if (studentAnswers[i] == correct[i]) {
+//         answers++;
+//       }
+//     }
+//     return answers;
+//   }
+   
+//   export function gradeQuiz(
+//     studentsAnswers: StudentQuiz[],
+//     correct: number[]
+//   ): number[] {
+//     let result: number[] = [];
+   
+//     for (let i = 0; i < studentsAnswers.length; i++) {
+//       const correctCount = gradeStudent(studentsAnswers[i].quizAnswers, correct);
+//       result.push(correctCount);
+//     }
+   
+//     return result;
+//   }
+//   //IMPLEMENT THIS
+   
+//   export function gradeQuizLabeled(
+//     students: StudentQuiz[],
+//     correct: number[]
+//   ): StudentResult[] {
+//     const result: StudentResult[] = [];
+//     for (let i = 0; i < students.length; i++) {
+//       const correctCount = gradeStudent(students[i].quizAnswers, correct);
+//       const stuResult = {
+//         id: students[i].id,
+//         score: correctCount,
+//       };
+//       result.push(stuResult);
+//     }
+   
+//     return result;
+//   }
+//   //IMPLEMENT THIS
+   
+
+
 export interface StudentQuiz {
-    id: number;
-    quizAnswers: number[];
+  studentId: number;
+  quizAnswers: number[];
+}
+
+export function gradeStudent(studentAnswers: number[],correct: number[]): number {
+  let correctAnswer = 0;
+  for (let i = 0; i < correct.length; i++) {
+    if (correct[i] === studentAnswers[i]) {
+      correctAnswer++;
+    }
   }
-   
-  export interface StudentResult {
-    id: number;
-    score: number;
-  }
-   
-  export const studentQuizzes: StudentQuiz[] = [];
-  export const CORRECT_ANSWERS: number[] = [1, 2, 3];
-  export function gradeStudent(
-    studentAnswers: number[],
-    correct: number[]
-  ): number {
-    let answers: number = 0;
-    for (let i = 0; i < studentAnswers.length; i++) {
-      if (studentAnswers[i] == correct[i]) {
-        answers++;
+  return correctAnswer;
+}
+
+export function gradeQuiz(student: StudentQuiz[], score: number[]): number[] {
+  let correctAnswers: number[] = [];
+  let count = 0;
+
+  for (let i = 0; i < student.length; i++) {
+    count = 0;
+    for (let j = 0; j < score.length; j++) {
+      if (student[i].quizAnswers[j] === score[j]) {
+        count++;
       }
     }
-    return answers;
+    correctAnswers.push(count);
   }
-   
-  export function gradeQuiz(
-    studentsAnswers: StudentQuiz[],
-    correct: number[]
-  ): number[] {
-    let result: number[] = [];
-   
-    for (let i = 0; i < studentsAnswers.length; i++) {
-      const correctCount = gradeStudent(studentsAnswers[i].quizAnswers, correct);
-      result.push(correctCount);
-    }
-   
-    return result;
+  return correctAnswers;
+}
+//IMPLEMENT THIS
+export interface ScoresId{
+    id?:number,
+    score?:number
+}
+export function gradeQuizLabeled(student: StudentQuiz[],score: number[]
+): ScoresId[] {
+  let correctAnswers: ScoresId[] = [];
+  let count = 0;
+ 
+
+  for (let i = 0; i < student.length; i++) {
+    count = 0;
+    for (let j = 0; j < score.length; j++) {
+      if (student[i].quizAnswers[j] === score[j]) {
+        count++;
+      }
+    } 
+    let studentScores: ScoresId = {id:student[i].studentId, score:count};
+    correctAnswers.push(studentScores);
   }
-  //IMPLEMENT THIS
-   
-  export function gradeQuizLabeled(
-    students: StudentQuiz[],
-    correct: number[]
-  ): StudentResult[] {
-    const result: StudentResult[] = [];
-    for (let i = 0; i < students.length; i++) {
-      const correctCount = gradeStudent(students[i].quizAnswers, correct);
-      const stuResult = {
-        id: students[i].id,
-        score: correctCount,
-      };
-      result.push(stuResult);
-    }
-   
-    return result;
-  }
-  //IMPLEMENT THIS
-   
+  return correctAnswers;
+}
 
